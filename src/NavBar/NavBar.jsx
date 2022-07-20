10af8cd75434fd8b3ca356f812ec924e263fcd4f
Ariel <102249863+ArielRivas@users.noreply.github.com>
2022-07-20 11:18:21 +0200

    adding navbar

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './NavBar.scss'
+import imageIcon from "../assets/controlRoom/Group 12-1.png"
 
 const NavBar = () => {
   return (
     <header className='container__NavBar'>
+       <span><img className='iconNavBar' src={imageIcon} alt="iconRocket"/>The Last Mission</span>
         <nav className='container__NavBar-navBar'>
-            <span><img className='container__NavBar-navBar-iconNavBar' src="../assets/controlRoom/Group 12-1.png" alt="iconRocket"/>The Last Mission</span>
-            <Link to="/controlroom">Control Room</Link>
+            <Link to="/" ><span className="navBar_Link">Planets</span></Link>
+            <Link to="/controlroom"><span className="navBar_Link">Control Room</span></Link>
         </nav>  
     </header>
   )
